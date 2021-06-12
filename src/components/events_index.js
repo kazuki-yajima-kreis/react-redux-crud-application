56d3825c55ee5e7d261be389ccd89c165439fc71
kazuki <test@gmail.com>
2021-06-12 23:37:33 +0900

    implement event new component

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import _ from 'lodash'
+import { Link } from 'react-router-dom'
 
 import { readEvents } from '../actions'
 
@@ -35,6 +36,8 @@ class EventsIndex extends Component {
           <tbody>
             {this.renderEvents()}
           </tbody>
+
+          <Link to="/events/new">New Event</Link>
         </table>
       </React.Fragment>
     )
